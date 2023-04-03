01ad593e52749f1b5ec399fb44eed4f982971b08
Bruno César <79723563+brunodeev@users.noreply.github.com>
2023-04-03 08:06:13 -0300

    array05

---
+++ Arrays_Slices/arrays05.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main(){
+  var matriz [3][2]int
+  for linha := 0; linha < len(matriz); linha++{
+    for coluna := 0; coluna < len(matriz[linha]); coluna++{
+      var x int
+      fmt.Printf("Digite o elemento da linha %d e coluna %d: ", linha, coluna)
+      fmt.Scan(&x)
+      matriz[linha][coluna] = x
+    }
+  }
+  fmt.Println(matriz)
+}
