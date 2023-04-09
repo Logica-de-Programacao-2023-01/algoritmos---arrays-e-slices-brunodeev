fd0ad0c757a1da67be465e72c926c42ff7f97b60
brunodeev <bruno.meireles@sempreceub.com>
2023-04-09 09:11:05 -0300

    array07

--- Arrays_Slices/arrays05.go
+++ Arrays_Slices/arrays05.go
@@ -2,15 +2,20 @@ package main
 
 import "fmt"
 
-func main(){
-  var matriz [3][2]int
-  for linha := 0; linha < len(matriz); linha++{
-    for coluna := 0; coluna < len(matriz[linha]); coluna++{
-      var x int
-      fmt.Printf("Digite o elemento da linha %d e coluna %d: ", linha, coluna)
-      fmt.Scan(&x)
-      matriz[linha][coluna] = x
-    }
-  }
-  fmt.Println(matriz)
+func main() {
+	var a, b, c, d, e, f int
+	fmt.Println("Qual o valor na linha 0, coluna 0?")
+	fmt.Scan(&a)
+	fmt.Println("Qual o valor da linha 0, coluna 1? ")
+	fmt.Scan(&b)
+	fmt.Println("Qual o valor da linha 1 coluna 0? ")
+	fmt.Scan(&c)
+	fmt.Println("Qual o valor da linha 1 coluna 1? ")
+	fmt.Scan(&d)
+	fmt.Println("Qual o valor da linha 2 coluna 0? ")
+	fmt.Scan(&e)
+	fmt.Println("Qual o valor da linha 2 coluna 1?")
+	fmt.Scan(&f)
+	Array := [3][2]int{{a, b}, {c, d}, {e, f}}
+	fmt.Println(Array)
 }
